9a732dc809aeda0b6791fd91ab27a7f7e2b23635
vendittogiovanni97 <vendittogiovanni97@hotmail.it>
2025-03-06 17:17:23 +0100

    aggiornamento message type

--- backend/src/types/wsMessagetype.ts
+++ backend/src/types/wsMessagetype.ts
@@ -1,14 +1,46 @@
-export type MessageType =
-  | "new-user"
-  | "join-call"
-  | "leave-call"
+export type CallMessageType =
   | "offer"
   | "answer"
-  | "ice-candidate";
+  | "ice-candidate"
+  | "join-call"
+  | "leave-call"
+  | "call-status"
+  | "invite-to-call"
+  | "mute"
+  | "screen-share";
 
-  export default interface WebsocketMessage {
-    type: MessageType,
-    username: string,
-    receiver?: string,
-    sender?: string
-  }
+export type SystemMessageType =
+  | "user-status"
+  | "new-user"
+  | "kick-user"
+  | "warning"
+  | "room-join"
+  | "room-leave";
+
+export type ChatMessageType =
+  | "send-message"
+  | "receive-message"
+  | "typing"
+  | "delete-message"
+  | "edit-message"
+  | "broadcast-message"
+  | "message-read";
+
+export type FileMediaMessageType =
+  | "file-transfer"
+  | "image-message"
+  | "video-message"
+  | "audio-message";
+
+export type MessageType =
+  | FileMediaMessageType
+  | ChatMessageType
+  | CallMessageType
+  | SystemMessageType;
+
+export default interface WebsocketMessage {
+  type: MessageType;
+  username: string;
+  receiver?: string;
+  sender?: string;
+}
