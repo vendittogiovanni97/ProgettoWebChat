3debf1f64f560f6281da492612790e25d4eb17d4
vendittogiovanni97 <vendittogiovanni97@hotmail.it>
2025-03-20 13:30:03 +0100

    aaa

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/Auth.Provider.tsx";
+import { CustomThemeProvider } from "./context/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <CustomThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </CustomThemeProvider>
   </StrictMode>
 );
